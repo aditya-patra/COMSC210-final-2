f4dfeaee93747327fb02dd3cb6b01c4b1661fecb
Aditya Patra <adityabpatra@gmail.com>
2025-12-08 17:40:47 -0800

    paste function to add customers

--- main.cpp
+++ main.cpp
@@ -69,6 +69,38 @@ int main() {
         head = head->next;
         delete serve;
         if ((int) (rand() % 2) > 0) {
+    for(const auto& order_pair : orders) {
+        int name_extracted = 0;
+        for(auto it = order_pair.begin(); it != order_pair.end(); it++) {
+            if (!name_extracted) {
+                name = *it;
+                name_extracted = 1;
+            }
+            else {
+                order = *it;
+            }
+        }
+        if (head != nullptr) {
+            Coffee* temp = head;
+            while (temp->next != nullptr) {
+                temp = temp->next;
+            }
+            Coffee* new_order = new Coffee;
+            new_order->name = name;
+            new_order->order = order;
+            temp->next = new_order;
+        }
+        else {
+            Coffee* new_order = new Coffee;
+            new_order->name = name;
+            new_order->order = order;
+            head = new_order;
+        }
+        cnt++;
+        if (cnt == 3) {
+            break;
+        }
+    }
         }
     }
 }
